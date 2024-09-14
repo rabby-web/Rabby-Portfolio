db63c687b3870094e26b404d8f617a77d0034e71
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-14 12:13:44 +0600

    footer componentss add

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/shared/NavBar";
+import Footer from "@/components/shared/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -38,6 +39,7 @@ export default function RootLayout({
         >
           <Navbar></Navbar>
           {children}
+          <Footer></Footer>
         </ThemeProvider>
       </body>
     </html>
---
+++ components/shared/Footer.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#222935]  md:p-6 p-6 text-center">This is footer</div>
+  );
+};
+
+export default Footer;
