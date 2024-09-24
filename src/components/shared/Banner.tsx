c2f337b2f0e5753ef3204fad9d2aaa4180bae1c8
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-24 13:12:52 +0600

    button add

--- src/components/shared/Banner.tsx
+++ src/components/shared/Banner.tsx
@@ -1,3 +1,5 @@
+import { IconCloudDemo } from "./BannerIcon";
+
 const Banner = () => {
   return (
     <div className="">
@@ -5,9 +7,56 @@ const Banner = () => {
         {/* Radial gradient for the container to give a faded look */}
         <div className=" pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <div className="max-w-7xl mx-auto ">
-          <p className="text-4xl sm:text-7xl font-bold  py-8">
-            This Is Banner Section
-          </p>
+          <div className="md:mt-20">
+            {/* grid */}
+            <div className="flex flex-col-reverse md:flex md:flex-row items-center py-4">
+              <div className="flex-1 text-center md:text-left space-y-2">
+                <h4 className="text-dark-01 dark:text-white">Hello, this is</h4>
+                <h2 className="text-dark-03 text-3xl md:text-5xl font-bold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+                  Zulkar Naeem Rabby
+                </h2>
+                <h2 className="dark:text-white text-3xl font-semibold">
+                  And Im{" "}
+                  <span className="dark:text-[#9C1291] text-[#134699]">
+                    Full Stack Developer
+                  </span>
+                </h2>
+                <div className="flex gap-2 justify-center items-center md:justify-start md:items-start mt-3">
+                  <a
+                    href="#_"
+                    className="relative inline-flex items-center justify-center  p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-xl shadow-2xl group"
+                  >
+                    <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
+                    <span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+                      <span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
+                      <span className="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
+                    </span>
+                    <span className="relative text-white"> About Me</span>
+                  </a>
+                  <a href="https://drive.google.com/uc?export=download&id=1M9xARPrlfW8IQtDjb4aNNIVpStLsEPuH">
+                    <a
+                      href="#_"
+                      className="relative inline-flex items-center justify-center  p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-xl shadow-2xl group"
+                    >
+                      <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
+                      <span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+                        <span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
+                        <span className="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
+                      </span>
+                      <span className="relative text-white">
+                        {" "}
+                        Download Resume
+                      </span>
+                    </a>
+                  </a>
+                  {/* <Download></Download> */}
+                </div>
+              </div>
+              <div className="flex-1">
+                <IconCloudDemo></IconCloudDemo>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white dark:bg-dark-bg-08 dark:text-white text-black sticky top-0 ">
+    <nav className="bg-white dark:bg-dark-bg-08 dark:text-white text-black sticky top-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
