7b0b2467303aff1d0e5ebac051f671ffbe65bdd6
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-26 09:03:24 +0600

    frontend all icon add to all section

--- src/components/shared/data/proficiencyData.js
+++ src/components/shared/data/proficiencyData.js
@@ -2,6 +2,16 @@ import htmlIcon from "../../../assets/icon/HTML5.svg";
 import cssIcon from "../../../assets/icon/CSS3.svg";
 import tailwindIcon from "../../../assets/icon/tailwind.svg";
 import bootstrapIcon from "../../../assets/icon/Bootstrap.svg";
+import javaScriptIcon from "../../../assets/icon/JavaScript.svg";
+import typeScriptIcon from "../../../assets/icon/TypeScript.svg";
+import reactIcon from "../../../assets/icon/React.svg";
+import nextJSIcon from "../../../assets/icon/Next.js.svg";
+import reduxIcon from "../../../assets/icon/Redux.svg";
+import antDesignIcon from "../../../assets/icon/Ant Design.svg";
+import shadCNIcon from "../../../assets/icon/shadcn.svg";
+import jestIcon from "../../../assets/icon/Jest.svg";
+import firebaseIcon from "../../../assets/icon/Firebase.svg";
+import framerIcon from "../../../assets/icon/framer.svg";
 
 export const icons = [
   {
@@ -26,10 +36,80 @@ export const icons = [
     category: "frontend",
   },
   {
-    id: 3,
+    id: 4,
     title: "Utility-First Framework",
     image: bootstrapIcon,
     name: "Bootstrap",
+    category: "frontend",
+  },
+  {
+    id: 5,
+    title: "Programming Language",
+    image: javaScriptIcon,
+    name: "JavaScript",
+    category: "frontend",
+  },
+  {
+    id: 6,
+    title: "Programming Language",
+    image: typeScriptIcon,
+    name: "TypeScript",
+    category: "frontend",
+  },
+  {
+    id: 7,
+    title: "JavaScript Library for UI",
+    image: reactIcon,
+    name: "React",
+    category: "frontend",
+  },
+  {
+    id: 8,
+    title: "React Framework",
+    image: nextJSIcon,
+    name: "Next JS",
+    category: "frontend",
+  },
+  {
+    id: 9,
+    title: "State Management Library",
+    image: reduxIcon,
+    name: "Redux",
+    category: "frontend",
+  },
+  {
+    id: 10,
+    title: "UI Component Library",
+    image: antDesignIcon,
+    name: "Ant Design",
+    category: "frontend",
+  },
+  {
+    id: 11,
+    title: "Component Library",
+    image: shadCNIcon,
+    name: "ShadCN",
+    category: "frontend",
+  },
+  {
+    id: 12,
+    title: "Testing Framework",
+    image: jestIcon,
+    name: "Jest",
+    category: "frontend",
+  },
+  {
+    id: 13,
+    title: "Firebase Auth",
+    image: firebaseIcon,
+    name: "Firebase",
+    category: "frontend",
+  },
+  {
+    id: 14,
+    title: "Animation Library",
+    image: framerIcon,
+    name: "Framer Motion",
     category: "frontend",
   },
 ];
