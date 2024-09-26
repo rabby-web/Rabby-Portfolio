a816a6bde6c1ff2b727ca53b7b5cb0dff3459926
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-26 07:54:38 +0600

    hover text add

--- src/components/shared/AllIcons.tsx
+++ src/components/shared/AllIcons.tsx
@@ -4,16 +4,31 @@ import { icons } from "./data/proficiencyData";
 const AllIcons = () => {
   return (
     <div>
-      <div className="grid grid-cols-3  md:grid-cols-4 lg:grid-cols-6 gap-2 ">
+      <div className="grid grid-cols-3  md:grid-cols-4 lg:grid-cols-6 gap-3 ">
         {icons?.map((icon) => (
-          <div key={icon.id} className="w-full p-4 border rounded-xl">
+          <div
+            key={icon.id}
+            className="w-full p-6 border rounded-xl text-center flex flex-col justify-center items-center relative group"
+          >
             <Image
               src={icon.image}
-              width={30}
-              height={30}
+              width={50}
+              height={50}
               alt={icon.name}
             ></Image>
-            <h2>{icon.title}</h2>
+            <h2 className="text-lg pt-3">{icon.name}</h2>
+            <p className="hidden absolute top-full -mt-72 text-base text-blue-700 group-hover:block bg-white p-3 border  rounded-xl">
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+                {icon.title}
+              </span>
+
+              <button
+                type="button"
+                className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-xl text-sm px-5 py-2.5 text-center me-2 mb-2 mt-3"
+              >
+                Explore More
+              </button>
+            </p>
           </div>
         ))}
       </div>
--- src/components/shared/data/proficiencyData.js
+++ src/components/shared/data/proficiencyData.js
@@ -1,25 +1,35 @@
+import htmlIcon from "../../../assets/icon/HTML5.svg";
+import cssIcon from "../../../assets/icon/CSS3.svg";
 import tailwindIcon from "../../../assets/icon/tailwind.svg";
+import bootstrapIcon from "../../../assets/icon/Bootstrap.svg";
 
 export const icons = [
   {
     id: 1,
-    title: "Tailwind CSS",
-    image: tailwindIcon,
-    name: "Tailwind CSS",
+    title: "Hypertext Markup Language",
+    image: htmlIcon,
+    name: "HTML5",
     category: "frontend",
   },
   {
     id: 2,
-    title: "Tailwind CSS",
+    title: "Style Sheet Language",
+    image: cssIcon,
+    name: "CSS3",
+    category: "frontend",
+  },
+  {
+    id: 3,
+    title: "Utility-First Framework",
     image: tailwindIcon,
     name: "Tailwind CSS",
     category: "frontend",
   },
   {
     id: 3,
-    title: "Tailwind CSS",
-    image: tailwindIcon,
-    name: "Tailwind CSS",
+    title: "Utility-First Framework",
+    image: bootstrapIcon,
+    name: "Bootstrap",
     category: "frontend",
   },
 ];
