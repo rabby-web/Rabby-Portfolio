aa9ff85763e603e1949d57b5eb917e629a3d4134
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-28 08:44:19 +0600

    some modifie code contact page

--- src/components/shared/ContactPage.tsx
+++ src/components/shared/ContactPage.tsx
@@ -22,7 +22,7 @@ const ContactPage = () => {
                 data-aos-duration="1500"
                 className="md:w-1/2"
               >
-                <h2 className="text-blue-700 dark:text-white font-semibold text-3xl md:text-4xl mb-2">
+                <h2 className="text-[#3CD1B8] dark:text-rose-600 font-semibold text-3xl md:text-4xl mb-2">
                   Subscribe now
                 </h2>
                 <div className="w-20 h-1.5 bg-dark-03 mb-5 ml-2"></div>
@@ -45,7 +45,7 @@ const ContactPage = () => {
                     id=""
                   />
                   <button
-                    className="py-3 px-10 text-white bg-blue-700 rounded inline-flex gap-3 items-center"
+                    className="py-3 px-10 text-white bg-[#3CD1B8] rounded inline-flex gap-3 items-center"
                     type="submit"
                   >
                     Subscribe <FaPaperPlane></FaPaperPlane>
@@ -65,13 +65,13 @@ const ContactPage = () => {
                 </h2>
                 <div className="w-20 h-1.5 bg-dark-03 dark:text-white mb-5 ml-2"></div>
                 <div className="flex gap-3 font-medium text-xl items-center ">
-                  <FaWhatsapp className="text-dark-03 dark:text-white text-3xl" />
+                  <FaWhatsapp className="text-dark-03 dark:text-[#3CD1B8] text-3xl" />
                   <a className="text-dark-01 dark:text-white" href="#">
-                    01590040222
+                    01540643211
                   </a>
                 </div>
                 <div className="flex gap-3 font-medium text-xl items-center ">
-                  <MdEmail className="text-dark-03 dark:text-white text-3xl" />
+                  <MdEmail className="text-dark-03 dark:text-[#3CD1B8] text-3xl" />
                   <a
                     className="text-dark-01 dark:text-white"
                     href="mailto:rabby-webdeveloper@gmail.com"
@@ -83,22 +83,22 @@ const ContactPage = () => {
                   <h2 className="text-dark-01 dark:text-white font-semibold text-3xl md:text-4xl mb-2 mt-2">
                     Social Link
                   </h2>
-                  <div className="text-dark-03 dark:text-white flex  gap-4 mt-4">
+                  <div className="text-dark-03 dark:text-[#3CD1B8]  flex  gap-4 mt-4">
                     <a href="https://github.com/rabby-web">
-                      <div className="flex items-center justify-center gap-2 border rounded px-2 py-1">
+                      <div className="flex items-center justify-center gap-2 border rounded px-2 py-1 hover:dark:text-rose-600 hover:dark:border-rose-600">
                         <FaGithub />
                         <h2>GitHub</h2>
                       </div>
                     </a>
                     <a href="https://www.linkedin.com/in/rabby-web/">
-                      <div className="flex items-center justify-center gap-2 border rounded px-2 py-1">
+                      <div className="flex items-center justify-center gap-2 border rounded px-2 py-1 hover:dark:text-rose-600 hover:dark:border-rose-600">
                         <FaLinkedin />
 
                         <h2>Linkedin</h2>
                       </div>
                     </a>
                     <a href="https://www.facebook.com/ridoy.babu.3538">
-                      <div className="flex items-center justify-center gap-2 border rounded px-2 py-1">
+                      <div className="flex items-center justify-center gap-2 border rounded px-2 py-1 hover:dark:text-rose-600 hover:dark:border-rose-600">
                         <FaFacebook />
                         <h2>Facebook</h2>
                       </div>
